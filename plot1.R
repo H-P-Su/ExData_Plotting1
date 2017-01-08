6ab120ac120739c854cd0c2d22bec16eca87843f
Hua Su <xtals8hua@gmail.com>
2017-01-07 23:13:54 -0500

    parsing data in plot1.R

---
+++ plot1.R
@@ -0,0 +1,12 @@
+# read the dataset
+# filter out all data except for data from 1/2/2007 and 2/2/2007
+# change the date and time formats
+
+all_data <- read.delim("household_power_consumption.txt", sep = ";", na.strings = "?")
+feb_data <- all_data[grep("^[12]/2/2007", all_data$Date),]
+rm(all_data)  # In case memory is an issue ?
+#names(feb_data)[1:2] <- c("old_date", "old_time")
+feb_data$date_time = strptime(paste(feb_data$Date, feb_data$Time), format="%d/%m/%Y %H:%M:%S")
+#feb_data <- feb_data %>% mutate(Date = dmy(old_date), Time = hms(old_time))
+
+# create plot
